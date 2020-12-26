63d323153bf84e027a73f514e566cdb6a099c339
AlfieriChou <alfierichou@gmail.com>
2020-12-26 16:09:47 +0800

    chore: update index.js

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const loadModels = require('@galenjs/core')
+const loadModels = require('@galenjs/base')
 const loadSequelizeModels = require('@galenjs/sequelize-models')
 const buildSwaggerDocs = require('@galenjs/swagger')
-const path = require('path')
 
 const mysql = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'alfieri',
-  database: 'test'
+  default: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'alfieri',
+    database: 'test'
+  },
+  clients: {
+    main: {}
+  }
 }
-
 const bootstrap = async () => {
-  const modelDirPath = path.join(process.cwd(), './app/models')
-  const { remoteMethods, modelSchemas, schemas } = await loadModels(modelDirPath)
+  const { remoteMethods, modelSchemas, schemas } = await loadModels({
+    workspace: process.cwd(),
+    modelPath: 'app/models'
+  })
   console.log('----remoteMethods----', JSON.stringify(remoteMethods, null, 2))
   console.log('----modelSchemas----', JSON.stringify(modelSchemas, null, 2))
   console.log('----schemas----', JSON.stringify(schemas, null, 2))
-  const db = await loadSequelizeModels(modelSchemas, { mysql })
+  const db = await loadSequelizeModels(modelSchemas, mysql)
   const data = await db.User.findOne({
     where: { phone: '13222221111' },
     include: ['roles']
