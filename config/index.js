09890f2d8cc920c787d9a4ceb792f7161127475e
AlfieriChou <alfierichou@gmail.com>
2021-01-21 16:43:40 +0800

    chore: koa application

---
+++ config/index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  port: 4000,
+  mysql: {
+    default: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'alfieri',
+      database: 'test'
+    },
+    clients: {
+      main: {}
+    }
+  },
+  influx: {
+    host: '127.0.0.1',
+    database: 'test'
+  },
+  redis: {
+    default: {
+      host: '127.0.0.1',
+      port: 6379,
+      password: '',
+      db: 2
+    },
+    clients: {
+      main: {
+        keyPrefix: 'main'
+      }
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,72 +1,82 @@
+const Koa = require('koa')
+const koaBody = require('koa-body')
+const koaLogger = require('koa-logger')
+const bodyParser = require('koa-bodyparser')
+const os = require('os')
+
 const loadModels = require('@galenjs/base')
 const loadSequelizeModels = require('@galenjs/sequelize-models')
 const createInfluxClient = require('@galenjs/influx')
-const buildSwaggerDocs = require('@galenjs/swagger')
+// const buildSwaggerDocs = require('@galenjs/swagger')
 const createRedisClient = require('@galenjs/redis')
+const buildRouter = require('@galenjs/router')
 
-const config = {
-  mysql: {
-    default: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'alfieri',
-      database: 'test'
-    },
-    clients: {
-      main: {}
-    }
-  },
-  influx: {
-    host: '127.0.0.1',
-    database: 'test'
-  },
-  redis: {
-    default: {
-      host: '127.0.0.1',
-      port: 6379,
-      password: '',
-      db: 2
-    },
-    clients: {
-      main: {
-        keyPrefix: 'main'
-      }
-    }
-  }
-}
+const config = require('./config')
+
+const app = new Koa()
+let pendingCount = 0
 
 const bootstrap = async () => {
   const { remoteMethods, modelSchemas, schemas } = await loadModels({
     workspace: process.cwd(),
     modelPath: 'app/models'
   })
-  console.log('----remoteMethods----', JSON.stringify(remoteMethods, null, 2))
-  console.log('----modelSchemas----', JSON.stringify(modelSchemas, null, 2))
-  console.log('----schemas----', JSON.stringify(schemas, null, 2))
-  const db = await loadSequelizeModels(modelSchemas, config.mysql)
-  const data = await db.User.findOne({
-    where: { phone: '13222221111' },
-    include: ['roles']
+  app.context.remoteMethods = remoteMethods
+  app.context.modelSchemas = modelSchemas
+  app.context.schemas = schemas
+  app.context.models = await loadSequelizeModels(modelSchemas, config.mysql)
+  app.context.influx = await createInfluxClient(modelSchemas, config.influx)
+  app.context.redis = await createRedisClient(config.redis)
+  const router = await buildRouter({ remoteMethods, modelSchemas })
+  app.use(async (ctx, next) => {
+    const start = Date.now()
+    pendingCount += 1
+    if (ctx.request.method === 'OPTIONS') {
+      ctx.response.status = 200
+    }
+    ctx.set('Access-Control-Allow-Origin', ctx.request.header.origin)
+    ctx.set('Access-Control-Allow-Credentials', true)
+    ctx.set('Access-Control-Max-Age', 86400000)
+    ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE')
+    ctx.set('Access-Control-Allow-Headers', 'x-requested-with, accept, origin, content-type')
+    try {
+      await next()
+      const duration = Date.now() - start
+      await ctx.influx.writePoints([{
+        measurement: 'response_time',
+        tags: { host: os.hostname() },
+        fields: { duration, path: ctx.path }
+      }])
+    } catch (err) {
+      ctx.status = err.status || 500
+      ctx.body = {
+        code: ctx.status,
+        message: err.message
+      }
+    } finally {
+      pendingCount -= 1
+      if (pendingCount === 0) {
+        ctx.app.emit('pendingCount0')
+      }
+    }
   })
-  console.log('-------user--------', JSON.stringify(data.toJSON(), null, 2))
-  const openApi = await buildSwaggerDocs({
-    title: 'galen API document',
-    version: 'v3',
-    description: 'Galen document'
-  }, { schemas, remoteMethods })
-  console.log('-------openApi-------', openApi)
-  const influx = await createInfluxClient(modelSchemas, config.influx)
-  const influxData = await influx.query(`
-    select * from response_time
-    where host = '127.0.0.1'
-    order by time desc
-    limit 10
-  `)
-  console.log('-------influx-------', influxData)
-  const redis = await createRedisClient(config.redis)
-  await redis.set('main', 'test', 'value', 3 * 60)
-  const redisData = await redis.get('main', 'test')
-  console.log('-------redis-------', redisData)
+
+  app.use(koaLogger())
+  app.use(koaBody({}))
+  app.use(bodyParser())
+  app.use(router.routes())
+  app.use(router.allowedMethods())
+
+  app.listen(config.port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`✅  The server is running at http://localhost:${config.port}`)
+  })
 }
 
 bootstrap()
+
+// const openApi = await buildSwaggerDocs({
+//   title: 'galen API document',
+//   version: 'v3',
+//   description: 'Galen document'
+// }, { schemas, remoteMethods })
