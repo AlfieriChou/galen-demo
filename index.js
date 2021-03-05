1691045394902cd8b370e9ccc5658361c6053643
AlfieriChou <alfierichou@gmail.com>
2021-03-05 14:56:03 +0800

    chore: update typeorm-models connections

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const bootstrap = async () => {
   })
   console.log('---schemas-->', modelSchemas)
   const { models, connections } = await loadModels(modelSchemas, config.typeorm)
-  const connection = connections.main
-  const data = await connection.getRepository(models.User).find({
+  const data = await connections.get('main').getRepository(models.User).find({
     nick_name: 'test'
   })
   console.log('------>', JSON.stringify(data, null, 2))
